e7edcaaf72293be04dce861f61986fb2c295c38a
Takashi Taniguchi <Takashi@takashi-macbook-pro.local>
2018-05-26 15:34:49 +0900

    edit tag table

--- db/migrate/20180526055551_create_tags.rb
+++ db/migrate/20180526055551_create_tags.rb
@@ -1,7 +1,7 @@
 class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
-      t.string :content null:false
+      t.string :content, null: false
 
       t.timestamps null: false
     end
