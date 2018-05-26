03b1cb559126484d5f65d2114a595a57c626b7ca
Takashi Taniguchi <Takashi@takashi-macbook-pro.local>
2018-05-26 17:26:10 +0900

    Add tags

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -8,6 +8,7 @@ class PrototypesController < ApplicationController
   def new
     @prototype = Prototype.new
     @prototype.captured_images.build
+
   end
 
   def create
@@ -34,7 +35,8 @@ class PrototypesController < ApplicationController
       :catch_copy,
       :concept,
       :user_id,
-      captured_images_attributes: [:content, :status]
+      captured_images_attributes: [:content, :status],
+      tags: [:content]
     )
   end
 end
