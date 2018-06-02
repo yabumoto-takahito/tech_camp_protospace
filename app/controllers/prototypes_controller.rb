5bd89c94698d06c42adea9b05f00118b21f6687d
yabumoto-takahito <yammo1107@yahoo.co.jp>
2018-06-02 16:04:51 +0900

    Edit prototypes_controller.rb
    updateアクションの実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:show, :edit]
+  before_action :set_prototype, only: [:show, :edit, :update]
 
   def index
     @prototypes = Prototype.all
@@ -23,6 +23,14 @@ class PrototypesController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to :root, notice: "prototype was successfully updated"
+    else
+      render :edit
+    end
   end
 
   private
