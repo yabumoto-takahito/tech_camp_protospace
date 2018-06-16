7865543358108aeab353463dd91fd3a8fe614f49
yabumoto-takahito <yammo1107@yahoo.co.jp>
2018-06-16 12:43:09 +0900

    リファクタリング

--- app/assets/javascripts/image-new.js
+++ app/assets/javascripts/image-new.js
@@ -27,17 +27,31 @@ $(function() {
 
   $('#prototype_captured_images_attributes_1_content').on('change', function(e) {
       var file = e.target.files[0];
-      // e.target.files[0]で選択されたファイル情報を取得
       var reader = new FileReader();
-      // ファイルを読み込むFileReaderオブジェクトを生成
 
       if(file.type.indexOf("image") < 0){
         aleart('画像ファイルを指定してください');
         return false;
       }
-      // 画像ファイル以外は処理を止める
+      reader.onload = function(e) {
+        $('#img_area1').empty();
+        $('#img_area1').attr({
+          src: e.target.result,
+          width: '100%',
+          height: '100%'
+          });
+      }
+      reader.readAsDataURL(file);
+  });
 
-      // 読み込み成功時に実行されるイベント
+  $('#prototype_captured_images_attributes_2_content').on('change', function(e) {
+      var file = e.target.files[0];
+      var reader = new FileReader();
+
+      if(file.type.indexOf("image") < 0){
+        aleart('画像ファイルを指定してください');
+        return false;
+      }
       reader.onload = function(e) {
         $('#img_area2').empty();
         $('#img_area2').attr({
@@ -45,25 +59,18 @@ $(function() {
           width: '100%',
           height: '100%'
           });
-        // 画像を表示するinputをempty(）で一旦空にする。そして先ほど取得したファイルのURLはevent.target.resultから取得できるので、それをsrc属性に指定。
       }
       reader.readAsDataURL(file);
-      // ファイル読み込みを実行
   });
 
-  $('#prototype_captured_images_attributes_2_content').on('change', function(e) {
+  $('#prototype_captured_images_attributes_3_content').on('change', function(e) {
       var file = e.target.files[0];
-      // e.target.files[0]で選択されたファイル情報を取得
       var reader = new FileReader();
-      // ファイルを読み込むFileReaderオブジェクトを生成
 
       if(file.type.indexOf("image") < 0){
         aleart('画像ファイルを指定してください');
         return false;
       }
-      // 画像ファイル以外は処理を止める
-
-      // 読み込み成功時に実行されるイベント
       reader.onload = function(e) {
         $('#img_area3').empty();
         $('#img_area3').attr({
@@ -71,35 +78,7 @@ $(function() {
           width: '100%',
           height: '100%'
           });
-        // 画像を表示するinputをempty(）で一旦空にする。そして先ほど取得したファイルのURLはevent.target.resultから取得できるので、それをsrc属性に指定。
       }
       reader.readAsDataURL(file);
-      // ファイル読み込みを実行
-  });
-
-  $('#prototype_captured_images_attributes_3_content').on('change', function(e) {
-      var file = e.target.files[0];
-      // e.target.files[0]で選択されたファイル情報を取得
-      var reader = new FileReader();
-      // ファイルを読み込むFileReaderオブジェクトを生成
-
-      if(file.type.indexOf("image") < 0){
-        aleart('画像ファイルを指定してください');
-        return false;
-      }
-      // 画像ファイル以外は処理を止める
-
-      // 読み込み成功時に実行されるイベント
-      reader.onload = function(e) {
-        $('#img_area4').empty();
-        $('#img_area4').attr({
-          src: e.target.result,
-          width: '100%',
-          height: '100%'
-          });
-        // 画像を表示するinputをempty(）で一旦空にする。そして先ほど取得したファイルのURLはevent.target.resultから取得できるので、それをsrc属性に指定。
-      }
-      reader.readAsDataURL(file);
-      // ファイル読み込みを実行
   });
 });
